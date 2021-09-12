6c1b52d7da1e77b3b0a499fd9482f0c2b941d6dc
tarekzoka <65197356+tarekzoka@users.noreply.github.com>
2021-09-13 00:48:59 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,15 +1,14 @@
 
- 
+Â 
 
  
 	#!/bin/sh	   
 	#	   
-	#command wget https://raw.githubusercontent.com/tarekzoka/
--scripts/main/installer.sh -O - | /bin/sh	   
-	
+	#command wget https://raw.githubusercontent.com/tarekzoka/-scripts/main/installer.sh -O - | /bin/sh
 	   
-	wget -O /var/volatile/tmp/script-1.0.tar.gz "https://raw.githubusercontent.com/tarekzoka/
--scripts/main/script-1.0.tar.gz"	   
+	
+	wget -O /var/volatile/tmp/script-1.0.tar.gz "https://raw.githubusercontent.com/tarekzoka/-scripts/main/script-1.0.tar.gz"
+
 	wait	   
 	tar xzvpf /tmp/*.tar.gz  -C /	   
 	wait	   
