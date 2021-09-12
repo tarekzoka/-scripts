581382280454193df5aa47c8287a0eab4b07075a
tarekzoka <65197356+tarekzoka@users.noreply.github.com>
2021-09-13 00:41:30 +0200

    Add files via upload

--- installer.sh
+++ installer.sh
@@ -7,7 +7,7 @@
 	#command wget https://raw.githubusercontent.com/tarekzoka/scripts/main/installer.sh -O - | /bin/sh	   
 	
 	   
-	wget -O /var/volatile/tmp/script-1.0.tar.gz "https://raw.githubusercontent.com/emilnabil/scripts/main/script-1.0.tar.gz"	   
+	wget -O /var/volatile/tmp/script-1.0.tar.gz "https://raw.githubusercontent.com/tarekzoka/scripts/main/script-1.0.tar.gz"	   
 	wait	   
 	tar xzvpf /tmp/*.tar.gz  -C /	   
 	wait	   
